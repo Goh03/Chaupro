5368bd8a7bf64e43f2274d65ff9f34fcfb012c07
GOH03 <tranbachau2003@gmail.com>
2024-09-29 13:46:18 +0700

    W3.final?

--- HW/W3/Prob_4/T_FF.v
+++ HW/W3/Prob_4/T_FF.v
@@ -1,12 +1,12 @@
 module T_FF (
-    T, clk, rst, Q
+    T, clk, rst, Q, Qn
 );
-    input T, clk, rst;
-    output Q;
-    wire D1, D2, Q_prev;
+    input T, clk, rst, Q;
+    output Qn;
+    wire D1, D2;
 
-    D_FF dff1 (D1, clk, rst, Q_prev);
-    xor gate1 (D2, T, Q_prev);
-    D_FF dff2 (D2, clk, rst, Q);
+    D_FF dff1 (D1, clk, rst, Q);
+    xor gate1 (D2, T, Q);
+    D_FF dff2 (D2, clk, rst, Qn);
 
 endmodule
